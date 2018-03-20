3a62ccf7a08893d135053a2e329702b148f7c243
Matias daneri <mdaneri98@gmail.com>
2018-03-20 18:02:54 -0300

    Implemented core home url

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django import views
 
 # Create your views here.
+class Home(views.View):
+
+    def get(self, request):
+        
+
+
+        return HttpResponse("Todo ok!")
